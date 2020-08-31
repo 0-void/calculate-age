8fa38c024d0e7e6e9474821474b6179ab014ae41
0-void <muthukumarperiyasamy.mk@gmail.com>
2020-08-31 09:10:01 +0530

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+const { logger } = require('./logger/log4js.config');
+const moment = require('moment');
+
+const calculateAge = date => {
+    const dob = new Date(new Date(date).getTime());
+    const today = new Date();
+    let age = today.getFullYear() - dob.getFullYear();
+    const dateDiff = today.getDate() - dob.getDate();
+
+    if (dateDiff) {
+        return age - 1;
+    }
+    return age;
+};
+
+const calculateAgeWithMoment = date => {
+    const dob = moment(new Date(date).getTime());
+    const today = moment(new Date());
+    return today.diff(dob, 'years');
+};
+
+logger.info('Vanilajs = ', calculateAge('12/26/1999'));
+logger.info('Momentjs = ', calculateAgeWithMoment('12/26/1999'));
